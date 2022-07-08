76099c27f5761a64d328b88107dda731a8fda5ef
Sriramjupally <102505008+Sriramjupally@users.noreply.github.com>
2022-07-08 10:28:57 +0530

    Create mean.c

---
+++ Final_Assignment/mean.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+
+    printf("%lf\n",mean("uni.dat"));
+    
+return 0;
+}
