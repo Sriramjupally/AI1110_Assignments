a264f1310dea7d5bf3c86ce862104ced9e7a6bb9
Sriramjupally <102505008+Sriramjupally@users.noreply.github.com>
2022-07-08 23:12:33 +0530

    Create mean_gau.c

---
+++ Final_Assignment/mean_gau.c
@@ -0,0 +1,19 @@
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+
+printf("%lf\n",mean("gau.dat"));
+
+
+return 0;
+
+}
