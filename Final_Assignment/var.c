7bc41964c071d14a1cfb1760b6110e968b481314
Sriramjupally <102505008+Sriramjupally@users.noreply.github.com>
2022-07-08 10:41:56 +0530

    Create var.c

---
+++ Final_Assignment/var.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+  //Var of uniform
+    printf("%lf\n",var("uni.dat"));
+    
+return 0;
+}
