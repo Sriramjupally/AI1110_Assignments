e3aaf44bc707629f333a287f2a197cf8f7fb14bd
Sriramjupally <102505008+Sriramjupally@users.noreply.github.com>
2022-07-08 23:13:03 +0530

    Create var_gau.c

---
+++ Final_Assignment/var_gau.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+
+	 printf("%lf\n",var("gau.dat"));
+	 return 0;
+}	
+	
+
